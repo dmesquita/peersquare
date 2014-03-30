7063e8ce18d022c672b7bfcad107770ab8180649
deh.mesquita@hotmail.com <deborah@deborah-Lenovo-IdeaPad-S400.(none)>
2014-03-30 17:41:20 -0300

    Tirando umas classes não utilizadas

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ Ext.application({
     ],
     
     stores: [
-		'Eventos', 'RuntimeVariables', 'NomeIdPracaAtual'
+		'Eventos', 'RuntimeVariables'
 	],
 	
 	controllers: [
--- app/store/NomeIdPracaAtual.js
+++
@@ -1,8 +0,0 @@
-Ext.define('PeerSquare.store.NomeIdPracaAtual', {
-	extend: 'Ext.data.Store',
-	requires: ['PeerSquare.model.NomeIdPracaAtual'],
-    config: {
-    	model: 'PeerSquare.model.NomeIdPracaAtual',
-    	autoLoad: true
-	}
-});
